8438453df809092beb278ef2795ec4856ca727ce
Idriss Tchoumba <tchoumbaidriss@gmail.com>
2022-11-13 13:11:14 +0100

    Debut projet

---
+++ Projet-de-Classe/src/main/java/proginfo/ecam03/Projet/de/Classe/ProjetDeClasseApplication.java
@@ -0,0 +1,13 @@
+package proginfo.ecam03.Projet.de.Classe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDeClasseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDeClasseApplication.class, args);
+	}
+
+}
---
+++ Projet-de-Classe/src/test/java/proginfo/ecam03/Projet/de/Classe/ProjetDeClasseApplicationTests.java
@@ -0,0 +1,13 @@
+package proginfo.ecam03.Projet.de.Classe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDeClasseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
